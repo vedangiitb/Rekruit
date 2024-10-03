e73fb31b90ae1b3238e23da9095216dede01bb80
vedangiitb <vedangbaleiitb@gmail.com>
2024-10-03 18:56:47 +0530

    added analytics section

--- src/Components/Home/HomeSection4.jsx
+++ src/Components/Home/HomeSection4.jsx
@@ -2,8 +2,17 @@ import React from "react";
 
 export default function HomeSection4() {
     return (
-        <div className="home-section-3">
-            {/* <h2 style= {{color:"#00289F"}}>Powerful Analytics</h2> */}
+        <div className="home-section-4">
+            <div className="sec-3-contents">
+                <h2 style={{ color: "#00289F" }}>Powerful AI-Powered Analytics</h2>
+                <h4 style={{ fontWeight: "600", color: "#00289F" }}>Code Quality Analysis</h4>
+                <p>Get a thorough Code Quality Analysis to evaluate the candidate's technical proficiency at every level. This includes key metrics like code efficiency, which measures how optimal their solutions are in terms of time and space complexity, as well as code readability, ensuring their code is well-structured, maintainable, and follows best practices</p>
+                <h4 style={{ fontWeight: "600", color: "#00289F" }}>Engagement & Behaviour Metrics</h4>
+                <p>Assess the critical soft skills of candidates using our detailed Engagement & Behavior Metrics. This feature allows you to measure how actively and effectively candidates engage during the interview process, tracking key behaviors such as focus, communication, and collaboration</p>
+                <h4 style={{ fontWeight: "600", color: "#00289F" }}>Benchmarking & Comparative Analytics</h4>
+                <p>Gain valuable insights into a candidate’s performance with Benchmarking & Comparative Analytics, allowing you to make data-driven hiring decisions with confidence. This feature compares each candidate’s performance against industry standards, peer groups, and your internal benchmarks. You can see how a candidate stacks up against others who have completed similar coding challenges, enabling you to identify top performers quickly</p>
+            </div>
+            <img className="sec-4-img" src="sec-4-img.png"></img>
         </div>
     )
 }
--- src/Layouts/Footer.jsx
+++ src/Layouts/Footer.jsx
@@ -2,28 +2,27 @@ import React from "react";
 
 export default function Footer() {
     return (
-        <div style={{ display: "flex", flexDirection: "column", backgroundColor: "black", color: "white" }}>
-            <div style={{ display: "flex", backgroundColor: "black", color: "white", paddingLeft: "15%", paddingRight: "15%", paddingTop: "5%", paddingBottom: "2%", justifyContent: "space-between" }} className="text">
-
+        <div style={{ display: "flex", flexDirection: "column", backgroundColor: "white", color: "black" }}>
+            <div style={{ display: "flex", backgroundColor: "white", color: "black", paddingLeft: "15%", paddingRight: "15%", paddingTop: "5%", paddingBottom: "2%", justifyContent: "space-between" }} className="text">
                 <div style={{ display: "flex", flexDirection: "column" }}>
                     <a href="/about" className="footer-item"><p>About</p></a>
-                    <p>Team</p>
-                    <p>Career</p>
-                    <p>Pricing</p>
+                    <a href="/team" className="footer-item"><p>Team</p></a>
+                    <a href="/careers" className="footer-item"><p>Career</p></a>
+                    <a href="/pricing" className="footer-item"><p>Pricing</p></a>
                 </div>
 
                 <div style={{ display: "flex", flexDirection: "column" }}>
-                    <p>FAQ</p>
+                    <a href="/faq" className="footer-item"><p>FAQ</p></a>
                     <a href="/contact" className="footer-item"><p>Contact</p></a>
-                    <p>Support</p>
-                    <p>Pricing</p>
+                    <a href="/support" className="footer-item"><p>Support</p></a>
+                    <a href="/pricing" className="footer-item"><p>Pricing</p></a>
                 </div>
 
                 <div style={{ display: "flex", flexDirection: "column" }}>
-                    <p>Legal</p>
-                    <p>Data Protection & Privacy</p>
-                    <p>T&C</p>
-                    <p>Impressum</p>
+                    <a href="/tnc" className="footer-item"><p>Legal</p></a>
+                    <a href="data-protection" className="footer-item"><p>Data Protection & Privacy</p></a>
+                    <a href="/tnc" className="footer-item"><p>T&C</p></a>
+                    <a href="/tnc" className="footer-item"><p>Impressum</p></a>
                 </div>
 
             </div>
