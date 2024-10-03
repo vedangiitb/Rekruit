1f2e79ec07261c29020b8652286e7cd0698fccfc
vedangiitb <vedangbaleiitb@gmail.com>
2024-10-03 20:46:57 +0530

    increased font size

--- src/Components/Home/HomeSection4.jsx
+++ src/Components/Home/HomeSection4.jsx
@@ -6,11 +6,11 @@ export default function HomeSection4() {
             <div className="sec-3-contents">
                 <h2 style={{ color: "#00289F" }}>Powerful AI-Powered Analytics</h2>
                 <h4 style={{ fontWeight: "600", color: "#00289F" }}>Code Quality Analysis</h4>
-                <p>Get a thorough Code Quality Analysis to evaluate the candidate's technical proficiency at every level. This includes key metrics like code efficiency, which measures how optimal their solutions are in terms of time and space complexity, as well as code readability, ensuring their code is well-structured, maintainable, and follows best practices</p>
+                <p>Get a comprehensive Code Quality Analysis to assess the candidate's technical skills at all levels. This includes key metrics like code efficiency, measuring solution optimization in time and space complexity, and code readability, ensuring well-structured, maintainable code that follows best practices.</p>
                 <h4 style={{ fontWeight: "600", color: "#00289F" }}>Engagement & Behaviour Metrics</h4>
                 <p>Assess the critical soft skills of candidates using our detailed Engagement & Behavior Metrics. This feature allows you to measure how actively and effectively candidates engage during the interview process, tracking key behaviors such as focus, communication, and collaboration</p>
                 <h4 style={{ fontWeight: "600", color: "#00289F" }}>Benchmarking & Comparative Analytics</h4>
-                <p>Gain valuable insights into a candidate’s performance with Benchmarking & Comparative Analytics, allowing you to make data-driven hiring decisions with confidence. This feature compares each candidate’s performance against industry standards, peer groups, and your internal benchmarks. You can see how a candidate stacks up against others who have completed similar coding challenges, enabling you to identify top performers quickly</p>
+                <p>Gain insights into a candidate’s performance with Benchmarking & Comparative Analytics, enabling confident, data-driven hiring decisions. This feature compares candidates against industry standards, peer groups, and internal benchmarks, helping you quickly identify top performers based on similar coding challenges.</p>
             </div>
             <img className="sec-4-img" src="sec-4-img.png"></img>
         </div>
